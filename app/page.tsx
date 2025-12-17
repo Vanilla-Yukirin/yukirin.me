2314bd93b634e9bf5b2c620022a8cfdff4f4dbd2
Vanilla <heshaohong2015@outlook.com>
2025-12-17 14:58:12 +0800

    feat: 暂时隐去页脚中的引用内容 - 上次只隐了 page/[page] 的

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,8 @@ export default async function HomePage() {
               {commitSha}
             </a>
           </p>
-          <p className={styles.quote}>&quot;Stay hungry, stay foolish.&quot;</p>
+          {/* 暂时隐去quote */}
+          {/* <p className={styles.quote}>&quot;Stay hungry, stay foolish.&quot;</p> */}
         </footer>
     </div>
   );
